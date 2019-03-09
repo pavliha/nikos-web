d8b60af1d2d4826d522e72cc97ca34e90b51dcc0
pavliha <kostyk.pavel.09@gmail.com>
2019-03-09 21:35:18 +0200

    add layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { object, array, shape, string } from 'prop-types'
 import loadable from '@loadable/component'
-import { Route, Switch } from 'react-router-dom'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import isEmpty from 'lodash/isEmpty'
+import IndexScene from './IndexScene'
+import Header from './Header'
+import connector from './connector'
+import preload from 'setup/utils/preload'
 
-const AuthLayout = loadable(() => import('./@auth/AuthLayout'))
+/**
+ * Util to prefetch data on server
+ */
 
-const Layout = () =>
-  <Switch>
-    <Route path="/auth" component={() => <AuthLayout />} />
-  </Switch>
+const AdminLayout = loadable(() => import('./@admin/AdminLayout'))
+const PricesScene = loadable(() => import('./@prices/PricesScene'))
 
-export default Layout
+/**
+ * Some dummy layout to show code splitting is working
+ */
+class Layout extends Component {
+
+  static getInitialData({ props }) {
+    const { actions } = props
+
+    return actions.photos.all()
+  }
+
+  componentDidMount() {
+    const { actions, photos } = this.props
+    if (isEmpty(photos)) {
+      actions.photos.all()
+    }
+
+  }
+
+  render() {
+
+    const { history } = this.props
+
+    return (
+      <Switch>
+        <Route path="/admin" component={() => <AdminLayout />} />
+        <Route path="/">
+          <div>
+            <Header pathname={history.location.pathname} />
+            <Switch>
+              <Route exact path="/" component={IndexScene} />
+              <Route exat path="/prices" component={() => <PricesScene />} />
+            </Switch>
+          </div>
+        </Route>
+      </Switch>
+    )
+  }
+}
+
+Layout.propTypes = {
+  actions: object.isRequired,
+  photos: array.isRequired,
+  history: shape({
+    location: shape({ pathname: string })
+  }).isRequired,
+}
+export default withRouter(connector(preload(Layout)))
