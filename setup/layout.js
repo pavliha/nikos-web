cbb0ee4ccd8559e72c03a4c3d1b016e336d27e09
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-04-27 22:35:49 +0300

    Update layout.js

--- setup/layout.js
+++ setup/layout.js
@@ -18,7 +18,7 @@ export default ({ content, extractor, state, sheetsRegistry }) => {
                 
         <!-- 1. Link to jQuery (1.8 or later), -->
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> <!-- 33 KB -->
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
         <!-- fotorama.css & fotorama.js. -->
         <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
         <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script> <!-- 16 KB -->
